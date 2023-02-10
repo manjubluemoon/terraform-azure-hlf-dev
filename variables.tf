1338f9fd7fa3f8ad53a9c594496add82422e1233
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-02-10 10:10:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,49 +1,40 @@
-variable "agent_count" {
-  default = 3
-}
-
-# The following two variable declarations are placeholder references.
-# Set the values for these variable in terraform.tfvars
-variable "aks_service_principal_app_id" {
-  default = ""
-}
-
-variable "aks_service_principal_client_secret" {
-  default = ""
-}
-
-variable "cluster_name" {
-  default = "k8stest"
-}
-
-variable "dns_prefix" {
-  default = "k8stest"
-}
-
-# Refer to https://azure.microsoft.com/global-infrastructure/services/?products=monitor for available Log Analytics regions.
-variable "log_analytics_workspace_location" {
-  default = "eastus"
-}
-
-variable "log_analytics_workspace_name" {
-  default = "testLogAnalyticsWorkspaceName"
-}
-
-# Refer to https://azure.microsoft.com/pricing/details/monitor/ for Log Analytics pricing
-variable "log_analytics_workspace_sku" {
-  default = "PerGB2018"
-}
-
 variable "resource_group_location" {
-  default     = "eastus"
-  description = "Location of the resource group."
+  default       = "South India"
+  description   = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "agent_count" {
+    default = 1
 }
 
 variable "ssh_public_key" {
-  default = "~/.ssh/id_rsa.pub"
+    default = "Enter_ssh_public_key"
+}
+
+variable "dns_prefix" {
+    default = "gritfyk8s"
+}
+
+variable cluster_name {
+    default = "gritfyk8s"
+}
+
+variable resource_group_name {
+    default = "gritfy"
+}
+
+variable locationk8s {
+    default = "South India"
+}
+
+variable aks_service_principal_app_id {
+    default= "Enter_aks_service_principal_app_id"
+}
+
+variable aks_service_principal_client_secret{
+    default = "Enter_aks_service_principal_client_secret"
+}
+
+variable aks_service_principal_object_id {
+    default= "Enter_aks_service_principal_object_id"
 }
