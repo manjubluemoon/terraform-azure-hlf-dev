51b18f7ca49e0b3f71d4bf7f36a10b569009a522
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-02-10 10:09:37 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,38 +1,28 @@
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
-  sensitive = true
+output "client_key" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
 }
 
-output "client_key" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
-  sensitive = true
+output "client_certificate" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
-  sensitive = true
-}
-
-output "cluster_password" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
-  sensitive = true
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
 }
 
 output "cluster_username" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
-  sensitive = true
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
 }
 
-output "host" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-  sensitive = true
+output "cluster_password" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
-  sensitive = true
+    value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+    sensitive = true
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "host" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
 }
