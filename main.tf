727225f24dc5b20991dce2a8ebb950d6f59dc8be
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-04-13 09:54:28 +0530

    Update main.tf
    updated the details like names and variables

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_kubernetes_cluster" "k8s" {
+resource "azurerm_kubernetes_cluster" "boochis-k8-hlf-dev" {
     name                = var.cluster_name
     location            = var.locationk8s
     resource_group_name = var.resource_group_name
     dns_prefix          = var.dns_prefix
 
     linux_profile {
-        admin_username = "ubuntu"
+        admin_username = "boss"
 
         ssh_key {
             key_data = var.ssh_public_key
