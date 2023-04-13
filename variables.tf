3a67f4f57e0c96fd2eb464b9744a487d72a953f4
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-04-13 16:57:13 +0530

    Update variables.tf
    removed duplicate rg name declaration

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "resource_group_name" {
-  default     = "rgboochis-hlf-dev-rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-
 variable "resource_group_location" {
   default       = "South India"
   description   = "Location of the resource group."
