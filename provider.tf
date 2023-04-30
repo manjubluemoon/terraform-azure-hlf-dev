2aca936427a4e3e3448111088df183825c1fb8cd
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-04-30 07:44:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,22 @@
-terraform {
-  required_version = ">=1.0"
+provider "azurerm" {
+  features {}
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "~>2.0"
-    }
-  }
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 provider "kubernetes" {
+  load_config_file       = false
+  host                   = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.host
+  username               = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.username
+  password               = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.password
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.cluster_ca_certificate)
+}
+
+provider "kubectl" {
   config_path = var.kube_config_path
 }
