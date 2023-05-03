b3550c3405d592a5815266bd182adce9bef75c49
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-03 16:08:52 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,39 +1,38 @@
-output "aks_cluster_name" {
-  description = "Name of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.name
+output "client_key" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.client_key
+    sensitive = true
 }
 
-output "aks_cluster_resource_group" {
-  description = "Name of the resource group that the AKS cluster is in."
-  value       = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.resource_group_name
+output "client_certificate" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.client_certificate
+    sensitive = true
 }
 
-output "aks_cluster_location" {
-  description = "Location of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.location
+output "cluster_ca_certificate" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.cluster_ca_certificate
+    sensitive = true
 }
 
-output "aks_cluster_kube_config" {
-  description = "Kube config for the AKS cluster."
-  value       = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config_raw
+output "cluster_username" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.username
+    sensitive = true
 }
 
-output "hyperledger_ca_username" {
-  description = "Username for the Hyperledger Fabric CA."
-  value       = var.ca_username
+output "cluster_password" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.password
+    sensitive = true
 }
 
-output "hyperledger_ca_password" {
-  description = "Password for the Hyperledger Fabric CA."
-  value       = var.ca_password
+output "kube_config" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config_raw
+    sensitive = true
 }
 
-output "hyperledger_orderer_address" {
-  description = "The address of the Hyperledger Fabric orderer."
-  value       = var.orderer_address
+output "host" {
+    value = azurerm_kubernetes_cluster.boochis-hlf-dev-k8s-cluster.kube_config.0.host
+    sensitive = true
 }
 
-output "hyperledger_peer_address" {
-  description = "The address of the Hyperledger Fabric peer."
-  value       = var.peer_address
+output "resource_group_name" {
+  value = azurerm_resource_group.boochis-hlf-dev-rg.name
 }
