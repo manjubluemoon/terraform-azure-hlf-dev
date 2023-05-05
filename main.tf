3135308cdbdef76b10bd1cf3d1b0e9f91a788e4a
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-05 11:06:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,44 +4,43 @@ resource "azurerm_resource_group" "boochis-hlf-dev-rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "boochis-hlf-dev-k8s-cluster" {
-    name                = var.cluster_name
-    location            = var.locationk8s
-    resource_group_name = azurerm_resource_group.boochis-hlf-dev-rg.name
-    dns_prefix          = var.dns_prefix
+  name                = var.cluster_name
+  location            = var.locationk8s
+  resource_group_name = azurerm_resource_group.boochis-hlf-dev-rg.name
+  dns_prefix          = var.dns_prefix
 
-    linux_profile {
-        admin_username = "boss"
+  linux_profile {
+    admin_username = "boss"
 
-        ssh_key {
-            key_data = var.ssh_public_key
-        }
+    ssh_key {
+      key_data = var.ssh_public_key
     }
+  }
 
-    default_node_pool {
-        name            = "default"
-        node_count      = var.agent_count
-        vm_size         = "Standard_D11"
-        os_disk_size_gb = 30
-    }
+  default_node_pool {
+    name            = "default"
+    node_count      = var.agent_count
+    vm_size         = "Standard_D11"
+    os_disk_size_gb = 30
+  }
 
-    service_principal {
-        client_id     = var.aks_service_principal_app_id
-        client_secret = var.aks_service_principal_client_secret
-    }
-
-# kubernetes_version = "1.25.0"
+  service_principal {
+    client_id     = var.aks_service_principal_app_id
+    client_secret = var.aks_service_principal_client_secret
+  }
 
   depends_on = [
     azurerm_resource_group.boochis-hlf-dev-rg
   ]
 
-    tags = {
-        Environment = "Development"
-    }
+  tags = {
+    Environment = "Development"
+  }
 }
 
 # Install and configure Helm
-provisioner "remote-exec" {
+resource "null_resource" "helm_installation" {
+  provisioner "remote-exec" {
     inline = [
       "kubectl apply -f ./helm-rbac.yaml",
       "helm init --service-account tiller"
