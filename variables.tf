bd2f11138c1da7d502f3aecce43f28059e388df9
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-06 19:40:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,64 +1,49 @@
-variable "resource_group_location" {
-  default       = "South India"
-  description   = "Location of the resource group."
+variable "agent_count" {
+  default = 3
 }
 
-variable "agent_count" {
-    default = 5
+# The following two variable declarations are placeholder references.
+# Set the values for these variable in terraform.tfvars
+variable "aks_service_principal_app_id" {
+  default = ""
+}
+
+variable "aks_service_principal_client_secret" {
+  default = ""
+}
+
+variable "cluster_name" {
+  default = "k8stest"
+}
+
+variable "dns_prefix" {
+  default = "k8stest"
+}
+
+# Refer to https://azure.microsoft.com/global-infrastructure/services/?products=monitor for available Log Analytics regions.
+variable "log_analytics_workspace_location" {
+  default = "eastus"
+}
+
+variable "log_analytics_workspace_name" {
+  default = "testLogAnalyticsWorkspaceName"
+}
+
+# Refer to https://azure.microsoft.com/pricing/details/monitor/ for Log Analytics pricing
+variable "log_analytics_workspace_sku" {
+  default = "PerGB2018"
+}
+
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "ssh_public_key" {
-    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDQJEaoMsy3Owj9OoTQcib1gGazU3tFd+Y7H99eGpqFYRqBBT1GkEEiEJUOsXz7yFag8N0jTzTGCHAwwCbpm7hg9hGC8JRxHF7Q7DLxUIeWkBKT4mW1YQulJu0cfqFX3x8x29ak2d7d4frgaUYoc1XMm9xNnZg9rlLDN8LH7PfcSMqXSY0kQilAJz3SmcDthG3bnSja8qcY1gvbkNalq/tLUkbud8DcQdqIGWtmyUh089yzIIeKnrUSv4zxMMGksOD8gd9FVGy4nqK3wCNEASUBB8DvI8NMfqSJIMWmqTYwJ1rYejbQbp4o3HmHl4MYkQRPvzcy4sSBBz0iqVDbzuM5TMR+Riw5aswhsKpeFwG9XFp1hlmfT0YH1SbX45KMgQWIDKZzPLgNWEwfMiPuA8KZfiPlX+5UZaKMsGG1TPmNCdB+hH/fBokqkFZ4wfDlJZ5jOSL4y6DCghBaDY2/fxZGIV3x5FjAuG7LUcE9yko8b57Yev/A62Pv7hJyOs86DU= generated-by-azure"
-}
-
-variable "SSH_PRIVATE_KEY" {}
-
-variable "dns_prefix" {
-    default = "boochis-hlf-dev-dns"
-}
-
-variable cluster_name {
-    default = "boochis-hlf-dev-cluster"
-}
-
-variable resource_group_name {
-    default = "boochis-hlf-dev-rg"
-}
-
-variable locationk8s {
-    default = "South India"
-}
-
-variable aks_service_principal_app_id {
-    default= "e9f91da8-c684-4cc4-b2d3-cabf7112e7c0"
-}
-
-variable client_secret{
-    default = "ePf8Q~doRQy3isQoZgNWuJlrkEKG9n0y679h8c_s"
-}
-
-variable subscription_id {
-    default= "931a682d-78da-480a-8768-6d37ac12dacf"
-}
-
-variable tenant_id {
-    default = "5768f076-4bcb-4145-90c6-17c03addad41"
-}
-
-variable aks_service_principal_object_id {
-    default= "c007453b-b40b-44cc-a1e5-d777a94f23ce"
-}
-
-variable "namespace" {
-  description = "The name of the Kubernetes namespace to create."
-  type        = string
-  default = "boochis-hlf-dev-ns"
-}
-
-variable client_id {
-    default= "e9f91da8-c684-4cc4-b2d3-cabf7112e7c0"
-}
-
-variable aks_service_principal_client_secret {
-    default = "ePf8Q~doRQy3isQoZgNWuJlrkEKG9n0y679h8c_s"
+  default = "~/.ssh/id_rsa.pub"
 }
