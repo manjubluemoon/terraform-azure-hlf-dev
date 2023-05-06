2331078c155480f025fa79d63aa758af5e732fd9
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-06 10:07:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -59,6 +59,6 @@ variable client_id {
     default= "e9f91da8-c684-4cc4-b2d3-cabf7112e7c0"
 }
 
-variable client_secret = var.aks_service_principal_client_secret {
+variable aks_service_principal_client_secret {
     default = "ePf8Q~doRQy3isQoZgNWuJlrkEKG9n0y679h8c_s"
 }
