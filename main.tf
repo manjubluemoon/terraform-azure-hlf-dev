39a2c806e1183ab3bcd8c44fb9b1508268733601
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-09 10:27:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,8 +83,5 @@ resource "azurerm_role_assignment" "boochis-hlf-dev-rbac" {
   role_definition_name             = "AcrPull"
   principal_id                     = azurerm_kubernetes_cluster.boochis-hlf-dev-cluster.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
-  
-  # Service principal arguments
-  service_principal_id     = var.service_principal_id
-  service_principal_secret = var.service_principal_secret
-}
+
+ }
