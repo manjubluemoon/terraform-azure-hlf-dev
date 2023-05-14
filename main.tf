87ccee21d7ee9af0f64ef15b5515c99542e579b2
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-14 06:06:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "azurerm_container_registry" "boochis-hlf-dev-acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.boochis-hlf-dev-rg.name
   location            = azurerm_resource_group.boochis-hlf-dev-rg.location
-  sku                 = "Standard"
+  sku                 = "Basic"
 }
 
 resource "azurerm_kubernetes_cluster" "boochis-hlf-dev-cluster" {
