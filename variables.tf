e882c25c28ef53c82978907bb10a64107a156faf
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-05-14 09:03:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,8 +43,8 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
-  default     = "rg"
+variable "resource_group_name" {
+  default     = "boochis-hlf-dev-rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
